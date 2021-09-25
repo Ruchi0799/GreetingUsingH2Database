1fdef5accf8fb6e56b346636b8e69bd952353a46
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-09-25 23:08:51 +0530

    [ruchi]: implemented view by id method from database

--- src/main/java/com/spring/databaseGreeting/controller/CustomerController.java
+++ src/main/java/com/spring/databaseGreeting/controller/CustomerController.java
@@ -1,11 +1,13 @@
 package com.spring.databaseGreeting.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,5 +45,16 @@ public class CustomerController {
 			// TODO: handle exception
 		}
 		
+	}
+	
+	
+
+	@GetMapping("/customers/{id}")
+	public ResponseEntity<Customer> getSingleCustomers(@PathVariable Long id){
+		Optional<Customer> customer=customerRepo.findById(id);
+		if(customer.isPresent()) {
+			return new ResponseEntity<Customer>(customer.get(),HttpStatus.OK);
+		}
+		return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
 	}
 }
